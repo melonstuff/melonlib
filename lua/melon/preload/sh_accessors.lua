d54a30eb030e79bd6156528d6965a40f93c9e5cd
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2022-01-26 16:30:21 -0800

    refactor: made accessortable accept existing table

--- lua/melon/preload/sh_accessors.lua
+++ lua/melon/preload/sh_accessors.lua
@@ -1,9 +1,10 @@
 
-function melon.AccessorTable()
-    return {
-        Accessor = function(s, name, default)
-            AccessorFunc(s, "val_" .. name, name)
-            s["val_" .. name] = default
-        end
-    }
+function melon.AccessorTable(tbl)
+    tbl = tbl or {}
+    tbl.Accessor = function(s, name, default)
+        AccessorFunc(s, "val_" .. name, name)
+        s["val_" .. name] = default
+    end
+
+    return tbl
 end
