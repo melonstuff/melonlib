a8bae9b1060a6e90e34c98bb8e2ab8122665ce1a
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2022-01-26 16:32:28 -0800

    feat: made accessortables automatically assign __index

--- lua/melon/preload/sh_accessors.lua
+++ lua/melon/preload/sh_accessors.lua
@@ -1,6 +1,7 @@
 
 function melon.AccessorTable(tbl)
     tbl = tbl or {}
+    tbl.__index = tbl
     tbl.Accessor = function(s, name, default)
         AccessorFunc(s, "val_" .. name, name)
         s["val_" .. name] = default
