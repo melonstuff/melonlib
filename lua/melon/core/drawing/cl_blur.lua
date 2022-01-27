dbd1e09948b302bec864121480f4f624282eb82d
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2022-01-26 16:11:34 -0800

    feat: added blur function

---
+++ lua/melon/core/drawing/cl_blur.lua
@@ -0,0 +1,17 @@
+
+local blurMat = Material("pp/blurscreen")
+function melon.DrawBlur(panel, localX, localY, w, h, passes)
+    if blurPassesNum == 0 then return end
+    local x, y = panel:LocalToScreen(localX, localY)
+    local scrw, scrh = ScrW(), ScrH()
+
+    surface.SetMaterial(blurMat)
+    surface.SetDrawColor(255, 255, 255)
+
+    for i = 0, (passes or 6) do
+        blurMat:SetFloat("$blur", i * .33)
+        blurMat:Recompute()
+    end
+    render.UpdateScreenEffectTexture()
+    surface.DrawTexturedRect(x * -1, y * -1, scrw, scrh)
+end
