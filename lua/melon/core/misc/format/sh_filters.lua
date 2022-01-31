cf323a26dbe3020e43e7b8e51533255991ad0948
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2022-01-30 17:46:58 -0800

    refactor: cleaned up filters file

--- lua/melon/core/misc/format/sh_filters.lua
+++ lua/melon/core/misc/format/sh_filters.lua
@@ -5,6 +5,11 @@ melon.string = melon.string or {}
 melon.string.filters = melon.string.filters or {}
 local f = melon.string.filters
 
+local function err(msg)
+    return "<Error: " .. msg .. ">"
+end
+
+-- String Ops
 function f.upper(str)
     return string.upper(str)
 end
@@ -13,36 +18,49 @@ function f.capitalize(str)
     return str:sub(1, 1):upper() .. str:sub(2, -1)
 end
 
-function f.int(str)
-    return tonumber(str) or "<Error: Invalid Number '" .. str .. "'>"
-end
-
+-- Num Ops
 function f.isplural(num)
     num = tonumber(num)
 
     if not num then
-        return "<Error: Invalid Number passed to isplural ('" .. num .. "')>"
+        return err("Invalid Number passed to isplural ('" .. num .. "')")
     end
 
     return num == 1 and "" or "s"
 end
 
-function f.call(func, ...)
-    if not isfunction(func) then
-        return "<Error: Attempted to call non-function '" .. tostring(func) .. "'>"
-    end
-
-    return func(...)
-end
-
 function f.round(num, places)
     local t = tonumber(num)
 
-    if not t then return "<Error: Attempted to round non-number '" .. tostring(num) ..  "'>" end
+    if not t then return err("Attempted to round non-number '" .. tostring(num) ..  "'") end
 
     return math.Round(num, places)
 end
 
 function f.comma(num)
     return string.Comma(num)
+end
+
+-- Func Ops
+function f.call(func, ...)
+    if not isfunction(func) then
+        return err("Attempted to call non-function '" .. tostring(func) .. "'")
+    end
+
+    return func(...)
+end
+
+-- Type Ops
+function f.int(str)
+    return math.Round(tonumber(str), 0) or err("Invalid Number '" .. str .. "'")
+end
+
+function f.str(val)
+    return tostring(val)
+end
+
+function f.prettycolor(col)
+    if not melon.IsColor(col) then
+        return err("Invalid Color '" .. tostring(col) .. "'")
+    end
 end
