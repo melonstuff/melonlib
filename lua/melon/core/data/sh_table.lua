e76ddf6055ea8304c9853611430bc9816d1c8a59
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2022-02-03 17:55:39 -0800

    feat: added table mapping

--- lua/melon/core/data/sh_table.lua
+++ lua/melon/core/data/sh_table.lua
@@ -1,8 +1,17 @@
 
-function melon.KV2VK(tbl)
+function melon.Map(tbl, func)
     local new = {}
+
     for k,v in pairs(tbl) do
-        new[v] = k
+        local nk, nv = func(k, v)
+        new[nk] = nv
     end
+
     return new
+end
+
+function melon.KV2VK(tbl)
+    return melon.Map(tbl, function(k, v)
+        return v, k
+    end )
 end
