18edf1fc8c7028f82f53f12675f320b1cfe68125
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2023-02-08 00:53:14 -0800

    feat: fixed up bshadows for colored shadows

--- lua/melon/core/thirdparty/cl_blues_shadows.lua
+++ lua/melon/core/thirdparty/cl_blues_shadows.lua
@@ -10,14 +10,6 @@ BSHADOWS.ShadowMaterial = CreateMaterial("bshadows","UnlitGeneric",{
     ["alpha"] = 1
 })
 
-BSHADOWS.ShadowMaterialGrayscale = CreateMaterial("bshadows_grayscale","UnlitGeneric",{
-    ["$translucent"] = 1,
-    ["$vertexalpha"] = 1,
-    ["$alpha"] = 1,
-    ["$color"] = "0 0 0",
-    ["$color2"] = "0 0 0"
-})
-
 local xx,yy,ww,hh = 0,0,0,0
 BSHADOWS.BeginShadow = function(x, y, w, h)
     render.PushRenderTarget(BSHADOWS.RenderTarget)
@@ -38,28 +30,24 @@ BSHADOWS.EndShadow = function(intensity, spread, blur, opacity, direction, dista
     distance = distance or 0
     _shadowOnly = _shadowOnly or false
 
-    render.CopyRenderTargetToTexture(BSHADOWS.RenderTarget2)
+    render.CopyRenderTargetToTexture(BSHADOWS.RenderTarget)
 
     if blur > 0 then
         render.OverrideAlphaWriteEnable(true, true)
-        render.BlurRenderTarget(BSHADOWS.RenderTarget2, spread, spread, blur)
+        render.BlurRenderTarget(BSHADOWS.RenderTarget, spread, spread, blur)
         render.OverrideAlphaWriteEnable(false, false) 
     end
 
     render.PopRenderTarget()
 
     BSHADOWS.ShadowMaterial:SetTexture("$basetexture", BSHADOWS.RenderTarget)
-    BSHADOWS.ShadowMaterialGrayscale:SetTexture("$basetexture", BSHADOWS.RenderTarget2)
+    BSHADOWS.ShadowMaterial:SetFloat("$alpha", opacity / 255)
     
-    local xOffset = math.sin(math.rad(direction)) * distance 
-    local yOffset = math.cos(math.rad(direction)) * distance
-
-    BSHADOWS.ShadowMaterialGrayscale:SetFloat("$alpha", opacity / 255)
-    render.SetMaterial(BSHADOWS.ShadowMaterialGrayscale)
-    for i = 1 , math.ceil(intensity / 2) do
+    render.SetMaterial(BSHADOWS.ShadowMaterial)
+    for i = 1 , math.ceil(intensity) do
         render.SetScissorRect(xx, yy, xx + ww, yy + hh, true)
-        render.DrawScreenQuad(xOffset, yOffset, ww, hh)
-        render.SetScissorRect(xx, yy, xx + ww, yy + hh, false)
+        render.DrawScreenQuad()
+        render.SetScissorRect(0, 0, 0, 0, false)
     end
 
     if not _shadowOnly then
