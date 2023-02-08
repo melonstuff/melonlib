15593f12e17a42cfd1fc7b4c330afd97d7591d8c
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2023-02-07 22:06:46 -0800

    feat: added drag and resize panels

---
+++ lua/melon/core/elements/cl_draggable.lua
@@ -0,0 +1,81 @@
+
+local PANEL = vgui.Register("Melon:Draggable", {}, "DPanel")
+AccessorFunc(PANEL, "area", "AreaOf") -- takes Panel
+AccessorFunc(PANEL, "bbp", "Bounded") -- takes Panel or true for parent
+
+function PANEL:Init()
+    self:SetBounded(true)
+    self:SetAreaOf(self)
+    self.dragging = false 
+end
+
+function PANEL:OnMousePressed(m)
+    if m == MOUSE_LEFT then
+        self:StartDragging()
+        self.dragging = {self:GetAreaOf():LocalCursorPos()}
+        return
+    end
+
+    self.dragging = false
+end
+
+function PANEL:OnMouseReleased(m)
+    if m == MOUSE_LEFT and self.dragging then
+        self:EndDragging()
+        self.dragging = false
+    end
+end
+
+function PANEL:CalcDragPos()
+    local x, y  = gui.MouseX(), gui.MouseY()
+    local xx,yy = self.dragging[1], self.dragging[2]
+
+    return x - xx, y - yy
+end
+
+function PANEL:CalcCroppedPos()
+    local x,y   = self:CalcDragPos()
+    local pw,ph = (
+        (ispanel(self:GetBounded()) and self:GetBounded()) or 
+        self:GetAreaOf():GetParent()):GetSize()
+    local mw,mh = self:GetAreaOf():GetSize()
+
+    return math.Clamp(x, 0, pw - mw), math.Clamp(y, 0, ph - mh)
+end
+
+function PANEL:Think()
+    if not self.dragging then return end
+
+    if not input.IsMouseDown(MOUSE_LEFT) or gui.IsConsoleVisible() or not system.HasFocus() then
+        self:EndDragging()
+        self.dragging = false
+
+        return
+    end
+
+    local x,y = self:CalcDragPos()
+
+    if self:GetBounded() then
+        x, y = self:CalcCroppedPos()
+    end
+
+    self:GetAreaOf():SetPos(x, y)
+end
+
+function PANEL:StartDragging()
+    self:SetCursor("sizeall")
+end
+
+function PANEL:EndDragging()
+    self:SetCursor("arrow")
+end
+
+melon.DebugPanel("DPanel", function(p)
+    p:SetSize(400, 400)
+    p:Center()
+
+    p.drag = vgui.Create("Melon:Draggable", p)
+    p.drag:SetSize(200, 200)
+    p.drag:Center()
+    p.drag:SetAreaOf(p)
+end )
---
+++ lua/melon/core/elements/cl_resizeable.lua
@@ -0,0 +1,106 @@
+
+
+local PANEL = vgui.Register("Melon:Resizable", {}, "DPanel")
+
+function PANEL:Init()
+    self:SetDragSize(12, 4)
+
+    self.dragging = false
+end
+
+function PANEL:SetMaxSize(w, h)
+    self.maxw, self.maxh = w, h
+end
+
+function PANEL:SetMinSize(w, h)
+    self.minw, self.minh = w, h
+end
+
+function PANEL:SetDragSize(size, pad)
+    self.dragsize = size
+    self.dragpad = pad or size / 6
+end
+
+function PANEL:WithinDragRegion(x, y)
+    local w, h = self:GetSize()
+    local s = self.dragsize + self.dragpad
+
+    return
+        (x > (w - s) and x < w) and
+        (y > (h - s) and y < h)
+end
+
+function PANEL:OnMousePressed(m)
+    if m != MOUSE_LEFT then 
+        self.dragging = false
+        return 
+    end
+
+    local x,y = self:LocalCursorPos()
+    if not self:WithinDragRegion(x, y) then
+        self.dragging = false
+        return
+    end
+
+    self:StartDragging()
+    self.dragging = {
+        x, y,
+        self:GetSize()
+    }
+end
+
+function PANEL:OnMouseReleased(m)
+    if m == MOUSE_LEFT and self.dragging then
+        self.dragging = false
+        self:EndDragging()
+    end
+end
+
+function PANEL:Think()
+    if not self.dragging then return end
+
+    if not input.IsMouseDown(MOUSE_LEFT) or gui.IsConsoleVisible() or not system.HasFocus() then
+        self:EndDragging()
+        self.dragging = false
+
+        return
+    end
+
+    local x,y = self:LocalCursorPos()
+    local xx,yy = self.dragging[1], self.dragging[2]
+    local w,h = self.dragging[3], self.dragging[4]
+    self:SetSize(
+        math.Clamp(w - (xx - x), self.minw, self.maxw), 
+        math.Clamp(h - (yy - y), self.minh, self.maxh)
+    )
+end
+
+function PANEL:StartDragging()
+    self:SetCursor("sizenwse")
+end
+
+function PANEL:EndDragging()
+    self:SetCursor("arrow")
+end
+
+function PANEL:PaintOver(w, h)
+    local size = self.dragsize
+    local pad = self.dragpad
+    melon.DrawImage("https://i.imgur.com/KRo8XD4.png", w - size - pad, h - size - pad, size, size)
+end
+
+function PANEL:StartDragging()
+    self:SetCursor("sizenwse")
+end
+
+function PANEL:EndDragging()
+    self:SetCursor("arrow")
+end
+
+melon.DebugPanel("Melon:Resizable", function(p)
+    p:SetSize(400, 400)
+    p:Center()
+
+    p:SetMaxSize(600, 600)
+    p:SetMinSize(200, 200)
+end )
