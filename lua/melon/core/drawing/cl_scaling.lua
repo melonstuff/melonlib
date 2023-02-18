03c71ff8254bde395283584d20c93501d74f6f75
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2023-02-18 01:00:32 -0800

    feat: added vararg scale func

--- lua/melon/core/drawing/cl_scaling.lua
+++ lua/melon/core/drawing/cl_scaling.lua
@@ -2,3 +2,13 @@
 function melon.Scale(v)
     return v * (ScrH() / 1080)
 end
+
+function melon.ScaleN(...)
+    local t = {...}
+
+    for k,v in pairs(t) do
+        t[k] = melon.Scale(v)
+    end
+
+    return unpack(t)
+end
