39d1f3695b58f377a3968da7a95fd53f6aae03f1
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2023-02-20 01:22:15 -0800

    refactor: more accurate profiling and stop argument

--- lua/melon/preload/sh_profile.lua
+++ lua/melon/preload/sh_profile.lua
@@ -1,9 +1,14 @@
 
-function melon.Profile(func, name)
+function melon.Profile(func, name, stop_profile)
+    if stop_profile then
+        return func
+    end
+
     return function(...)
         local start = SysTime()
         local ret = func(...)
-        melon.Log(0, "Finished Profiling Function '{1}', ran in {2}s", name or "unknown", math.Round(SysTime() - start, 2))
+        local endd = SysTime()
+        melon.Log(0, "Finished Profiling Function '{1}', ran in {2}s", name or "unknown", endd - start)
         return ret
     end
 end
