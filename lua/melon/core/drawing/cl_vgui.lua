641d5009b2a601de40c0bf14fc23f7a43bce5812
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2023-07-14 21:46:06 -0700

    feat: added vgui methods, breaks super old code

--- lua/melon/core/drawing/cl_vgui.lua
+++ lua/melon/core/drawing/cl_vgui.lua
@@ -1,64 +1,39 @@
 
 ----
----@deprecated
 ---@module
 ---@name melon.panels
 ---@realm CLIENT
 ----
----- Old panel registering library, we are modern men now, dumb idea.
+---- Misc panel helpers
 ----
+
 melon.panels = melon.panels or {}
-melon.panels.list = melon.panels.list or {}
 
-function melon.panels.New(name, base)
-    if not name then return end
-    base = base or "Panel"
+function melon.panels.DebugPaint(pnl, w, h)
+    melon.DrawBlur(pnl, 0, 0, w, h, 5)
 
-    local PANEL = {}
-    PANEL.__info = {
-        name = name,
-        base = base,
-        trace = debug.getinfo(2)
-    }
+    surface.SetDrawColor(22, 22, 22)
+    surface.DrawOutlinedRect(0, 0, w, h, 3)
 
-    PANEL.Init = function(s)
-        s:SetSize(100, 100)
-    end
-    PANEL.Paint = function(s,w,h) -- Just a base debug paint function
-        surface.SetDrawColor(225, 126, 9)
-        surface.DrawRect(0,0,w,h)
-        surface.SetDrawColor(255,255,255)
-        surface.DrawOutlinedRect(0,0,w,h,2)
+    surface.SetDrawColor(melon.colors.Rainbow())
+    surface.DrawOutlinedRect(1, 1, w - 2, h - 2)
+    surface.DrawLine(1, 1, w - 2, h - 2)
 
-        local _w, _h = draw.Text({
-            text = s.__info.name,
-            pos = {6, 6},
-            font = melon.Font(20)
-        })
+    surface.SetDrawColor(22, 22, 22)
+    surface.DrawLine(2, 3, w - 3, h - 2)
+    surface.DrawLine(3, 2, w - 2, h - 3)
 
-        local newh = 6 + _h + 2
-        surface.DrawRect(6, newh, _w, 1)
-        newh = newh + 2
-
-        local ww, wh = draw.Text({
-            text = w,
-            pos = {6, newh},
-            font = melon.Font(30)
-        })
-
-        local xw = draw.Text({
-            text = "x",
-            pos = {6 + ww, newh + wh - 5},
-            font = melon.Font(15),
-            yalign = 4
-        })
-
-        draw.Text({
-            text = h,
-            pos = {6 + ww + xw, newh},
-            font = melon.Font(30)
-        })
-    end
-
-    return vgui.Register(name, PANEL, base)
+    draw.Text({
+        text = pnl.ClassName or "anonPanel",
+        pos = {w / 2, h / 2},
+        xalign = 1,
+        yalign = 1,
+        font = melon.Font(40)
+    })
 end
+
+melon.DebugPanel("DPanel", function(pnl)
+    pnl:SetSize(500,500)
+    pnl:Center()
+    pnl.Paint = melon.panels.DebugPaint
+end )
