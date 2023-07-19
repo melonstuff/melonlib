8bbeb604c5e2b8514c41bc0acaf8bf11da90d709
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2023-07-19 01:48:58 -0700

    SWITCH TO MELONSTUFF ORG

--- lua/melon/core/misc/sv_update.lua
+++ lua/melon/core/misc/sv_update.lua
@@ -31,34 +31,53 @@ end )
 
 melon.Log(melon.UpdateLog, "Checking for updates")
 
-local url = "https://raw.githubusercontent.com/garryspins/melonlib/main/lua/autorun/sh_melon_lib_init.lua"
-melon.http.Get(url, function(bod)
-    local ver_text = string.gmatch(bod, "melon.version = \"([^\"]+)")()
-    local v = melon.ParseVersion(
-        ver_text
-    )
-    local ov = melon.ParseVersion(melon.version)
+----
+---@internal
+---@name melon.CheckForUpdates
+----
+function melon.CheckForUpdates(url, err)
+    melon.http.Get(url, function(bod)
+        local ver_text = string.gmatch(bod, "melon.version = \"([^\"]+)")()
+        local v = melon.ParseVersion(
+            ver_text
+        )
+        local ov = melon.ParseVersion(melon.version)
+    
+        if not v then
+            err(true)
+            return
+        end
+    
+        if v.major > ov.major then
+            melon.Log(melon.UpdateLog, "New release update found, please update ASAP (-> {1})", ver_text)
+        end
+    
+        if v.minor > ov.minor then
+            melon.Log(melon.UpdateLog, "New feature update found, update whenever you can (-> {1})", ver_text)
+        end
+    
+        if v.patch > ov.patch then
+            melon.Log(melon.UpdateLog, "New patch update found, update only if needed (-> {1})", ver_text)
+        end
+    
+        melon.Log(melon.UpdateLog, "Youre all up to date!", melon.version)
+    end, function()
+        err()
+    end )
+end
 
-    if not v then
-        melon.Log(melon.UpdateLog, "Could not find a valid version from the given text ({1})", ver_text)
+local init_url = "https://raw.githubusercontent.com/melonstuff/melonlib/main/lua/autorun/sh_melon_lib_init.lua"
+local backup   = "https://raw.githubusercontent.com/garryspins/melonlib/main/lua/autorun/sh_melon_lib_init.lua"
+melon.CheckForUpdates(init_url, function()
+    melon.CheckForUpdates(backup, function(bad)
+        if bad then
+            melon.Log(melon.UpdateLog, "Could not find a valid version from the given text ({1})", ver_text)
+            melon.Log(1, "Read previous error regarding Updating")
+        end
+
+        melon.Log(melon.UpdateLog, "Couldnt connect to github repository to find newer version ({1}), please report issue", init_url)
         melon.Log(1, "Read previous error regarding Updating")
-        return
-    end
+    end )
 
-    if v.major > ov.major then
-        melon.Log(melon.UpdateLog, "New release update found, please update ASAP (-> {1})", ver_text)
-    end
-
-    if v.minor > ov.minor then
-        melon.Log(melon.UpdateLog, "New feature update found, update whenever you can (-> {1})", ver_text)
-    end
-
-    if v.patch > ov.patch then
-        melon.Log(melon.UpdateLog, "New patch update found, update only if needed (-> {1})", ver_text)
-    end
-
-    melon.Log(melon.UpdateLog, "Youre all up to date!", melon.version)
-end, function()
-    melon.Log(melon.UpdateLog, "Couldnt connect to github repository to find newer version ({1}), please report issue", url)
-    melon.Log(1, "Read previous error regarding Updating")
+    melon.Log(melon.UpdateLog, "You should never see this post-transfer.")
 end )
