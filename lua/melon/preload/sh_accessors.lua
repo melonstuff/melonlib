299a96fe9cb67757a322d3f92e9eb8e4b6802562
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2023-08-07 23:38:24 -0700

    feat: add improved accessortable

--- lua/melon/preload/sh_accessors.lua
+++ lua/melon/preload/sh_accessors.lua
@@ -36,3 +36,29 @@ end
 ---@alias melon.AccessorTable
 ---- 
 melon.AT = melon.AccessorTable
+
+----
+---@name melon.AF
+----
+---@arg (table: table) Table to add the accessor to
+---@arg (name: string) String name for the accessor
+---@arg (default: any) Default value for the accessor
+----
+---- Good replacement for AccessorFunc
+----
+function melon.AF(t, name, default)
+    t["v_" .. name] = default
+
+    t[name] = function(s, v)
+        if v == nil then
+            return s["v_" .. name]
+        end
+
+        s["v_" .. name] = v
+        return s
+    end
+
+    return function(...)
+        melon.AF(t, ...)
+    end
+end
