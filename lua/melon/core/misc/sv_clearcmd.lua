97d63ac10e4576aaf151981d1aa9856259fa4a3f
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2023-11-07 17:08:19 -0800

    fix: clearable on client

--- lua/melon/core/misc/sv_clearcmd.lua
+++ lua/melon/core/misc/sv_clearcmd.lua
@@ -5,6 +5,9 @@
 ----
 ---- "Clears" the console by spamming it with a bunch of empty space
 ----
-concommand.Add("cleer", function()
+concommand.Add("cleer", function(ply)
+    if IsValid(ply) then return end
+    if CLIENT then return end
+    
     melon.clr()
 end)
