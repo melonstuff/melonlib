8a5470b81907ef32e80bfea6fd1d3332ff61964f
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2023-11-07 17:26:32 -0800

    perf: remove strange __index experiment i forgort

--- lua/melon/preload/sh_index_behavior.lua
+++ lua/melon/preload/sh_index_behavior.lua
@@ -12,12 +12,12 @@ function melon.DefineNewBehavior(key, fn)
     behavior[key] = fn
 end
 
-setmetatable(melon, {
-    __index = function(s, k)
-        if behavior[k] then
-            return behavior[k]()
-        end
+-- setmetatable(melon, {
+--     __index = function(s, k)
+--         if behavior[k] then
+--             return behavior[k]()
+--         end
 
-        return rawget(s, k)
-    end
-})
+--         return rawget(s, k)
+--     end
+-- })
