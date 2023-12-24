281e4426aedd9024e9f264e06e55f4466933efb6
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2023-12-23 22:35:53 -0600

    feat: scrollers

---
+++ lua/melon/core/elements/scroller/cl_scrollbar.lua
@@ -0,0 +1,243 @@
+
+----
+---@panel Melon:ScrollBar
+---@name melon.elements.ScrollBar
+----
+---@accessor (ScrollAmount:       number) Current scroll pos in pixels
+---@accessor (Canvas:              Panel) Current canvas panel
+---@accessor (Horizontal:        boolean) Is the scrollbar horizontal
+---@accessor (OverflowAmount:     number) How much is the scrollbar currently overflowing, negative if up
+---@accessor (iRealScrollAmount:  number) Internal, current real scroll position, interpolated
+---@accessor (iGrabbed:            table) Internal, grab data
+---@accessor (iGripColor:          Color) Internal, grip color
+---@accessor (iPanning:          boolean) Internal, are we panning?
+----
+---- Scrollbar for ScrollPanels, handles the actual scrolling logic
+----
+local PANEL = vgui.Register("Melon:ScrollBar", {}, "Panel")
+AccessorFunc(PANEL, "ScrollAmount", "ScrollAmount")
+AccessorFunc(PANEL, "Canvas", "Canvas")
+AccessorFunc(PANEL, "Horizontal", "Horizontal")
+AccessorFunc(PANEL, "OverflowAmount", "OverflowAmount")
+AccessorFunc(PANEL, "iRealScrollAmount", "iRealScrollAmount")
+AccessorFunc(PANEL, "iGrabbed", "iGrabbed")
+AccessorFunc(PANEL, "iGripColor", "iGripColor")
+AccessorFunc(PANEL, "iPanning", "iPanning")
+
+function PANEL:Init()
+    self.Grip = vgui.Create("Panel", self)
+
+    self:SetScrollAmount(0)
+    self:SetiRealScrollAmount(0)
+    self:SetiGripColor(Color(0, 0, 0, 160))
+    self:SetiPanning(false)
+
+    function self.Grip.OnMousePressed(s, m)
+        if m == MOUSE_LEFT then
+            self:Grab()
+        end
+    end
+end
+
+function PANEL:Paint(w, h)
+    if self:GetDomain() == 0 then return end
+  
+    local amt = math.Clamp(math.abs(self:GetOverflowAmount() or 0), 0, (self:GetHorizontal() and h or w) * 2)
+     
+    melon.masks.Start()
+        draw.RoundedBox(melon.Scale(4), self.Grip:GetX(), self.Grip:GetY(), self.Grip:GetWide(), self.Grip:GetTall(), self:GetiGripColor())
+    melon.masks.Source()
+        surface.SetDrawColor(255,255,255)
+
+        if self:GetHorizontal() then
+            surface.SetMaterial(melon.Material("vgui/gradient-l"))
+            surface.DrawTexturedRect(0, 0, amt, h)
+            surface.SetMaterial(melon.Material("vgui/gradient-r"))
+            surface.DrawTexturedRect(w - amt + 1, 0, amt, h)
+        else
+            surface.SetMaterial(melon.Material("vgui/gradient-u"))
+            surface.DrawTexturedRect(0, 0, w, amt)
+            surface.SetMaterial(melon.Material("vgui/gradient-d"))
+            surface.DrawTexturedRect(0, h - amt + 1, w, amt)
+        end
+
+    melon.masks.End(melon.masks.KIND_STAMP)
+end
+
+function PANEL:OnMousePressed(m)
+    if m == MOUSE_MIDDLE and self:GetParent():GetPanningEnabled() then
+        self:GetParent():Pan()
+    end
+
+    if m != MOUSE_LEFT then return end
+
+    local x, y = self:LocalCursorPos()
+    local r = self:GetHorizontal() and (x / self:GetWide()) or (y / self:GetTall())
+
+    self:SetScrollAmount(math.Clamp(r, 0, 1) * (self:GetHorizontal() and self:GetCanvas():GetWide() or self:GetCanvas():GetTall()) - ((self:GetHorizontal() and self.Grip:GetWide() or self.Grip:GetTall()) / 2))
+end
+
+function PANEL:PerformLayout(w, h)
+    if not IsValid(self:GetCanvas()) then return end
+
+    local cvs = self:GetCanvas()
+
+    local sw, sh = self:GetParent():GetSize()
+    local cw, ch = cvs:GetSize()
+
+    self.Grip:SetSize(w, h)
+
+    if self:GetHorizontal() then
+        self.Grip:SetWide(w * (sw / cw))
+    else
+        self.Grip:SetTall(h * (sh / ch))
+    end
+end
+
+function PANEL:GrabThink()
+    local grab = self:GetiGrabbed()
+
+    if grab then
+        if (not self:GetiPanning() and not input.IsMouseDown(MOUSE_LEFT)) then
+            self:SetiGrabbed(nil)
+        elseif self:GetiPanning() and (not input.IsMouseDown(MOUSE_MIDDLE)) then
+            self:SetiPanning(false)
+            self:SetiGrabbed(nil)
+        end
+
+        local pos = self:GetHorizontal() and (self.Grip:GetX() - grab.x) or (self.Grip:GetY() - grab.y)
+        local nx, ny = self:LocalCursorPos()
+
+        self:SetScrollAmount(math.Clamp((self:GetHorizontal() and nx or ny) + pos, -self:GetParent():GetScrollbarMaxOverflow(), self:GetDomain() + self:GetParent():GetScrollbarMaxOverflow()))
+    end
+end
+
+function PANEL:ScrollThink()
+    if not self:GetCanvas() then return end
+    if self:GetDomain() == 0 then return end
+
+    self:GrabThink()
+
+    self:SetScrollAmount(
+        Lerp(FrameTime() * 10, self:GetScrollAmount(), math.Clamp(self:GetScrollAmount(), 0, self:GetDomain()))
+    )
+
+    local rounded = math.Round(self:GetScrollAmount())
+    if rounded == 0 then
+        self:SetScrollAmount(0)
+    elseif rounded == self:GetDomain() then
+        self:SetScrollAmount(self:GetDomain())
+    end
+
+    self:SetOverflowAmount(self:GetScrollAmount() - math.Clamp(self:GetScrollAmount(), 0, self:GetDomain()))
+
+    local real = self:GetiRealScrollAmount()
+    self:SetiRealScrollAmount(Lerp(FrameTime() * 10, real, self:GetScrollAmount()))
+
+    local pos = (self:GetiRealScrollAmount() / self:GetDomain()) * (self:GetHorizontal() and (self:GetWide() - self.Grip:GetWide()) or (self:GetTall() - self.Grip:GetTall()))
+    
+    if self:GetHorizontal() then
+        self.Grip:SetX(pos)
+    else
+        self.Grip:SetY(pos)
+    end
+
+    if self:GetHorizontal() then
+        self:GetCanvas():SetX(-(self:GetiRealScrollAmount()))
+    else
+        self:GetCanvas():SetY(-(self:GetiRealScrollAmount()))
+    end
+end
+
+function PANEL:Think()
+    self:ScrollThink()
+end
+
+function PANEL:OnMouseWheeled(d)
+    self:AddScroll(d)
+end
+
+----
+---@method
+---@name melon.elements.ScrollBar:AddScroll
+----
+---@arg (delta:   number) Scroll delta to add
+---@arg (pixels: number?) Optional, how many pixels to scroll instead of delta
+----
+---- Scrolls the scrollbar however much is specified
+----
+function PANEL:AddScroll(delta, amt_in_pixels)
+    if not self:GetCanvas() then return end
+    if self:GetDomain() == 0 then return end
+    
+    self:SetScrollAmount(
+        math.Clamp(self:GetScrollAmount() + (amt_in_pixels or self:GetParent():GetScrollPerDelta() * (-delta)), -self:GetParent():GetScrollbarMaxOverflow(), self:GetDomain() + self:GetParent():GetScrollbarMaxOverflow())
+    )
+end
+
+----
+---@method
+---@name melon.elements.ScrollBar:ScrollTo
+----
+---@arg (pos: number) Position to scroll to
+----
+---- Scrolls to the given coordinate, if horizontal than X, if vertical than Y
+----
+function PANEL:ScrollTo(coord)
+    if not self:GetCanvas() then return end
+    if self:GetDomain() == 0 then return end
+    
+    self:SetScrollAmount(math.Clamp(coord, 0, self:GetHorizontal() and self:GetCanvas():GetWide() or self:GetCanvas():GetTall()))
+end
+
+----
+---@method
+---@name melon.elements.ScrollBar:ScrollToChild
+----
+---@arg (pnl: Panel) Panel to scroll to
+----
+---- Scrolls to a child panel, if the panel isnt a child then silently fails
+----
+function PANEL:ScrollToChild(panel)
+    if not self:GetCanvas() then return end
+    if self:GetDomain() == 0 then return end
+    
+    if not self:GetCanvas():IsOurChild(panel) then return end
+    
+    self:SetScrollAmount(math.Clamp(self:GetHorizontal() and panel:GetX() or panel:GetY(), 0, self:GetHorizontal() and self:GetCanvas():GetWide() or self:GetCanvas():GetTall()))
+end
+
+----
+---@internal
+---@method
+---@name melon.elements.ScrollBar:Grab
+----
+---- Initiates grip grabbing
+----
+function PANEL:Grab()
+    local x, y = self.Grip:LocalCursorPos()
+    self:SetiGrabbed({
+        x = x,
+        y = y
+    })
+end
+
+----
+---@method
+---@name melon.elements.ScrollBar:GetDomain
+----
+---@return (num: number) The domain
+----
+---- Returns the "Domain", the actual scrollable area of the canvas, so (canvas_size - parent_size)
+----
+function PANEL:GetDomain()
+    if not IsValid(self:GetCanvas()) then return end
+
+    if self:GetHorizontal() then
+        return self:GetCanvas():GetWide() - self:GetParent():GetWide()
+    end
+
+    return self:GetCanvas():GetTall() - self:GetParent():GetTall()
+end
+
+melon.DebugNamed("ScrollPanel")
---
+++ lua/melon/core/elements/scroller/cl_scrollpanel.lua
@@ -0,0 +1,138 @@
+
+melon.elements = melon.elements or {}
+
+----
+---@panel Melon:ScrollPanel
+---@name melon.elements.ScrollPanel
+----
+---@accessor (ScrollPerDelta:           number) How many pixels to scroll per-scroll wheel delta, unscaled
+---@accessor (ScrollbarSize:            number) How wide should the scrollbars be?
+---@accessor (ScrollbarPad:             number) How much padding should the scrollbars have
+---@accessor (ScrollbarMaxOverflow:     number) How much overflow in pixels should we allocate to the scrollbars, unscaled 
+---@accessor (PanningEnabled:          boolean) Should we allow panning on the scrollbars with middle mouse button? note you need to call :Pan() yourself
+---@accessor (Canvas:                    Panel) Canvas panel
+---@accessor (VerticalScrollEnabled:   boolean) Should we allow vertical scrolling?
+---@accessor (HorizontalScrollEnabled: boolean) Should we allow horizontal scrolling?
+----
+---- A general purpose unstyled scrollpanel rewrite containing over-content scrollbars, smooth scrolling and canvas based scrolling.
+----
+local PANEL = vgui.Register("Melon:ScrollPanel", {}, "Panel")
+AccessorFunc(PANEL, "ScrollPerDelta", "ScrollPerDelta")
+AccessorFunc(PANEL, "ScrollbarSize", "ScrollbarSize")
+AccessorFunc(PANEL, "ScrollbarPad", "ScrollbarPad")
+AccessorFunc(PANEL, "ScrollbarMaxOverflow", "ScrollbarMaxOverflow")
+AccessorFunc(PANEL, "PanningEnabled", "PanningEnabled")
+AccessorFunc(PANEL, "Canvas", "Canvas")
+AccessorFunc(PANEL, "VerticalScrollEnabled", "VerticalScrollEnabled")
+AccessorFunc(PANEL, "HorizontalScrollEnabled", "HorizontalScrollEnabled")
+
+melon.elements.ScrollPanel = PANEL
+
+function PANEL:SetCanvas(cvs)
+    self.Canvas = cvs
+
+    cvs:SetParent(self)
+    self:InvalidateLayout(true)
+
+    self.VScroll:SetCanvas(cvs)
+    self.HScroll:SetCanvas(cvs)
+
+    self.VScroll:SetZPos(2)
+    self.HScroll:SetZPos(2)
+end
+
+function PANEL:SetVerticalScrollEnabled(en)
+    self.VerticalScrollEnabled = en
+
+    self.VScroll:SetVisible(en)
+    self.VScroll:SetMouseInputEnabled(en)
+end
+
+function PANEL:SetHorizontalScrollEnabled(en)
+    self.HorizontalScrollEnabled = en
+
+    self.HScroll:SetVisible(en)
+    self.HScroll:SetMouseInputEnabled(en)
+end
+
+function PANEL:Init()
+    self.VScroll = vgui.Create("Melon:ScrollBar", self)
+    self.HScroll = vgui.Create("Melon:ScrollBar", self)
+    self.HScroll:SetHorizontal(true)
+
+    self:SetScrollPerDelta(127)
+    self:SetScrollbarSize(8)
+    self:SetScrollbarPad(4)
+    self:SetScrollbarMaxOverflow(250)
+    self:SetPanningEnabled(false)
+
+    self:SetVerticalScrollEnabled(true)
+    self:SetHorizontalScrollEnabled(true)
+end
+
+function PANEL:PerformLayout(w, h)
+    local size = melon.Scale(self:GetScrollbarSize())
+    local pad = melon.Scale(self:GetScrollbarPad())
+
+    self.VScroll:SetSize(size, h - (pad * 2))
+    self.VScroll:SetPos(w - size - pad, pad)
+
+    self.HScroll:SetSize(w - ((self:GetVerticalScrollEnabled() and self.VScroll:GetDomain() != 0) and (size + (pad * 3)) or (pad * 2)), size)
+    self.HScroll:SetPos(pad, h - size - pad)
+end
+
+function PANEL:OnMouseWheeled(d)
+    if (
+        (self.VScroll:IsHovered() and self.VScroll:GetDomain() != 0) or (self.HScroll:IsHovered() and self.HScroll:GetDomain() != 0)
+    ) or (
+        self.VScroll:IsChildHovered() or self.HScroll:IsChildHovered()
+    ) then return end
+
+    if self:GetHorizontalScrollEnabled() and (input.IsShiftDown() or self.VScroll:GetDomain() == 0) then
+        return self.HScroll:OnMouseWheeled(d)
+    end
+
+    self.VScroll:OnMouseWheeled(d)
+end
+
+function PANEL:OnMousePressed(m)
+    if m == MOUSE_MIDDLE then
+        self:Pan()
+    end
+end
+
+----
+---@method
+---@name melon.elements.ScrollPanel:Pan
+----
+---- Start panning, should only be called while MOUSE_MIDDLE is down
+---- Automatically abides by the PanningEnabled accessor
+----
+function PANEL:Pan()
+    if not self:GetPanningEnabled() then
+        return
+    end
+
+    self.VScroll:SetiPanning(true)
+    self.VScroll:Grab()
+
+    if self:GetHorizontalScrollEnabled() then
+        self.HScroll:SetiPanning(true)
+        self.HScroll:Grab()
+    end
+end
+
+melon.DebugNamed("ScrollPanel", function()
+    melon.DebugPanel(PANEL, function(p)
+        local canvas = vgui.Create("DPanel", p)
+
+        p:SetCanvas(canvas)
+        canvas:SetSize(p:GetWide() * 1, p:GetTall() * 5)
+
+        for i = 0, 40 do
+            local b = vgui.Create("DButton", canvas)
+            b:Dock(TOP)
+            b:SetText(i)
+        end
+    end )
+end )
