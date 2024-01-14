fd780e4a42d4ed1acadbdd8f9a560c296033040f
Garry but Spinning <64482863+garryspins@users.noreply.github.com>
2024-01-14 00:28:57 -0600

    feat: blame panel suite

--- lua/melon/core/elements/panel_suite/cl_ps_tools.lua
+++ lua/melon/core/elements/panel_suite/cl_ps_tools.lua
@@ -27,12 +27,40 @@ function PANEL:Init()
         else
             print("missing ready")
         end
-    end 
+    end
+
+    self.opened_from = {}
+end
+
+function PANEL:SanitizePath(path)
+    return path
+        :gsub("^addons/", "")
+        :gsub("^melonlib/", "")
+        :gsub("^lua/", "")
+        :gsub("^melon/", "")
 end
 
 function PANEL:SetSuitePanel(pnl)
     self.SuitePanel = pnl
     self.tabs.tabs[self.tabs:GetActiveTab()]:Ready(pnl)
+
+    self.opened_from = {}
+    local init, paint = (pnl.Init and debug.getinfo(pnl.Init) or {}), (pnl.Paint and debug.getinfo(pnl.Paint) or {})
+
+    init = self:SanitizePath(init.short_src or "")
+    paint = self:SanitizePath(paint.short_src or "")
+
+    if not string.StartsWith(init, "derma/") then
+        table.insert(self.opened_from, init)
+    end
+
+    if not string.StartsWith(paint, "derma/") then
+        table.insert(self.opened_from, paint)
+    end
+
+    if init == paint and #self.opened_from == 2 then
+        self.opened_from = {init}
+    end
 end
 
 function PANEL:PerformLayout(w, h)
@@ -40,7 +68,6 @@ function PANEL:PerformLayout(w, h)
     self.topbar:SetTall(melon.Scale(30))
     self.tabs:Dock(FILL)
 end
-
 function PANEL:Paint(w, h)
     surface.SetDrawColor(melon.PanelDevSuite.Theme.Background)
     surface.DrawRect(0, 0, w, h)
@@ -50,6 +77,21 @@ function PANEL:PaintOver(w, h)
     local oc = DisableClipping(true)
         surface.SetDrawColor(melon.PanelDevSuite.Theme.Accent)
         surface.DrawOutlinedRect(-1, -1, w + 2, h + 2)
+
+        local y = h + 4
+        
+        for k,v in pairs(self.opened_from) do
+            local _, th = draw.TextShadow({
+                text = v,
+                pos = {w / 2, y},
+                xalign = 1,
+                yalign = 3,
+                font = melon.Font(12, "Inter"),
+                color = {r = 255, g = 255, b = 255, a = 60}
+            }, 2, 100)
+
+            y = y + th
+        end
     DisableClipping(oc)
 end
 
