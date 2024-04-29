c79740985e115b50024cc13dedb98270cf68a23e
jackkdev <jackmccally1@gmail.com>
2024-04-28 23:15:32 -0500

    feat: added new table iteration functions

--- lua/melon/core/data/sh_table.lua
+++ lua/melon/core/data/sh_table.lua
@@ -1,16 +1,38 @@
-
 ----
 ---@name melon.Map
 ----
----@arg    (iter: generator) The iterator to map. Expected to be an iterator which returns a key and a value.
----@arg    (fn: func)        The mapping function. The function is passed the value then the key, and is expected to return a new value.
----@return (new: table)      A table with the mapped elements. 
+---@arg    (tbl: table) Table to map
+---@arg    (fn:   func) Function that takes k,v and returns a new k,v
+---@return (new: table) Table created by the mapper
 ----
----- Maps an iterator to a new table, calling `fn` with every key/value pair.
+---- Maps a table to a new table, calling func with every key and value.
 ----
-function melon.Map(iter, fn)
+function melon.Map(tbl, func)
     local new = {}
-    for key, value in iter do
+
+    for k,v in pairs(tbl) do
+        local nk, nv = func(k, v)
+        new[nk] = nv
+    end
+
+    return new
+end
+
+----
+---@name melon.JMap
+----
+---@arg    (iter: func) The iterator function to use.
+---@arg    (t: table)   The table to map.
+---@arg    (fn: func)   The mapping function. The function is passed the value then the key, and is expected to return a new value.
+---@return (new: table) A table with the mapped elements. 
+----
+---- Implements the JavaScript style map function.
+----
+---- Maps a table to a new table, calling `fn` with every key/value pair.
+----
+function melon.JMap(iter, t, fn)
+    local new = {}
+    for key, value in iter(t) do
         new[key] = fn(value, key)
     end
     return new
@@ -19,16 +41,17 @@ end
 ----
 ---@name melon.Reduce
 ----
----@arg    (iter: generator)    The iterator to map. Expected to be an iterator which returns a key and a value.
+---@arg    (iter: func)         The iterator function to use.
+---@arg    (t: table)           The table to reduce.
 ---@arg    (fn: func)           The reducer function. The function is passed the current value, the value, the key, and returns the next value.
 ---@arg    (initial_value: any) The initial value passed to `fn`.
 ---@return (new: any)           The reduced value.
 ----
 ---- Performs a reduction on the given `iter` with the given `fn`.
 ----
-function melon.Reduce(iter, fn, initial_value)
+function melon.Reduce(iter, t, fn, initial_value)
     local current_value = initial_value
-    for key, value in iter do
+    for key, value in iter(t) do
         current_value = fn(current_value, value, key)
     end
     return current_value
@@ -37,17 +60,15 @@ end
 ----
 ---@name melon.KV2VK
 ----
----@arg    (iter: generator) Iterator to convert.
----@return (new: table)      Converted table.
+---@arg    (tbl: table) Table to convert
+---@return (new: table) Converted table
 ----
 ---- Inverts a tables keys and values ([k] = v) into ([v] = k) for every pair in the given table.
 ----
-function melon.KV2VK(iter)
-    local o = {}
-    for key, value in iter do
-        o[value] = key
-    end
-    return o
+function melon.KV2VK(tbl)
+    return melon.Map(tbl, function(k, v)
+        return v, k
+    end)
 end
 
 ----
