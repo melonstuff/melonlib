415d174deaadb71c128c75c393dd876b7a234d60
garryspins <orionspersonal1954@gmail.com>
2026-02-16 11:40:40 -0500

    doc: melon.char.*

--- lua/melon/core/extensions/sh_char.lua
+++ lua/melon/core/extensions/sh_char.lua
@@ -1,6 +1,20 @@
 
+----
+---@realm SHARED
+---@name melon.char
+----
+---- Contains useful functions for matching characters against sets of characters efficiently
+----
 melon.char = melon.char or {}
 
+----
+---@name melon.char.Set
+----
+---@arg    (chars: char...) All specific characters to match against
+---@return (match: fn(char) -> bool) The generated function
+----
+---- Creates a new function that checks against the given characters
+----
 function melon.char.Set(...)
     local set = {}
     for k, v in pairs({...}) do
@@ -12,6 +26,15 @@ function melon.char.Set(...)
     end
 end
 
+----
+---@name melon.char.Range
+----
+---@arg    (from: char) The starting character
+---@arg    (to:   char) The ending character
+---@return (match: fn(char) -> bool) The generated function
+----
+---- Creates a new function that checks if the given char is within the two given codepoints inclusively
+----
 function melon.char.Range(from, to)
     to = isstring(to) and string.byte(to) or to
     from = isstring(from) and string.byte(from) or from
@@ -23,6 +46,16 @@ function melon.char.Range(from, to)
     end
 end
 
+----
+---@name melon.char.Ranges
+----
+---@arg    (ranges: table<fn(char) -> bool>) Table of functions to check against
+---@arg    (intersects: bool) Should the checking requirement match only if its in all ranges?
+---@return (match: fn(char) -> bool) The generated function
+----
+---- Creates a new function that checks if the given character is in the given ranges  
+---- If intersects is true, checks if its in all ranges
+----
 function melon.char.Ranges(ranges, intersects)
     return function(val)
         for _, v in pairs(ranges) do
@@ -43,20 +76,70 @@ function melon.char.Ranges(ranges, intersects)
     end
 end
 
-
+----
+---@name melon.char.IsUpper
+----
+---@arg    (char) The character to check
+---@return (bool) Is this character in the given set
+----
+---- Is the given character an Alphabetical Upper-Case 
+----
 melon.char.IsUpper = melon.char.Range("A", "Z")
+
+----
+---@name melon.char.IsLower
+----
+---@arg    (char) The character to check
+---@return (bool) Is this character in the given set
+----
+---- Is the given character an Alphabetical Lower-Case 
+----
 melon.char.IsLower = melon.char.Range("a", "z")
+
+----
+---@name melon.char.IsAlpha
+----
+---@arg    (char) The character to check
+---@return (bool) Is this character in the given set
+----
+---- Is the given character an Alphabetic character 
+----
 melon.char.IsAlpha = melon.char.Ranges({
     melon.char.IsUpper,
     melon.char.IsLower
 })
 
+----
+---@name melon.char.IsNum
+----
+---@arg    (char) The character to check
+---@return (bool) Is this character in the given set
+----
+---- Is the given character a Numeric character 
+----
 melon.char.IsNum = melon.char.Range("0", "9")
+
+----
+---@name melon.char.IsAlphaNumeric
+----
+---@arg    (char) The character to check
+---@return (bool) Is this character in the given set
+----
+---- Is the given character an AlphaNumeric character
+----
 melon.char.IsAlphaNumeric = melon.char.Ranges({
     melon.char.IsAlpha,
     melon.char.IsNum
 })
 
+----
+---@name melon.char.IsKeyboardSymbol
+----
+---@arg    (char) The character to check
+---@return (bool) Is this character in the given set
+----
+---- Is the given character a "keyboard symbol", aka any character enterable through a standard US keyboard
+----
 melon.char.IsKeyboardSymbol = melon.char.Set(
     "!", "@", "#", "$", "%", "^", "&", "*",
     "(", ")", "[", "]", "{", "}", "<", ">",
@@ -65,6 +148,14 @@ melon.char.IsKeyboardSymbol = melon.char.Set(
     "-", "_", "=", "+"
 )
 
+----
+---@name melon.char.IsHex
+----
+---@arg    (char) The character to check
+---@return (bool) Is this character in the given set
+----
+---- Is the given character a hexadecimal character, (0-9, a-f, A-F)
+----
 melon.char.IsHex = melon.char.Ranges({
     melon.char.IsNum,
     melon.char.Range("a", "f"),
