403459f6e0bc63b230d678aa3697b30829b9ce2a
garryspins <orionspersonal1954@gmail.com>
2026-03-03 22:28:07 -0500

    refactor: melon.AccessorFunc

--- lua/melon/preload/sh_accessors.lua
+++ lua/melon/preload/sh_accessors.lua
@@ -1,75 +1,5 @@
 
 ----
----@deprecated melon.AccessorFunc
----@name melon.AccessorTable
-----
----- Dont use this
-----
-function melon.AccessorTable(tbl, metatable)
-    tbl = tbl or {}
-    tbl.__index = tbl
-    tbl.Accessor = function(s, name, default)
-        AccessorFunc(s, "val_" .. name, name)
-        s["val_" .. name] = default
-    end
-    tbl.New = function(s, ...)
-        local m = setmetatable({}, s.__metatable)
-
-        for k,v in pairs(s) do
-            m[k] = v
-        end
-
-        if m.Init then
-            m:Init(...)
-        end
-
-        return m
-    end
-
-    tbl.__metatable = metatable or {}
-    setmetatable(tbl, tbl.__metatable)
-    return tbl
-end
-
----- 
----@silence
----@name  melon.AT
----@alias melon.AccessorTable
----- 
-melon.AT = melon.AccessorTable
-
-----
----@deprecated melon.AccessorFunc
----@name melon.AF
-----
----@arg (table: table) Table to add the accessor to
----@arg (name: string) String name for the accessor
----@arg (default: any) Default value for the accessor
-----
----- Good replacement for AccessorFunc
-----
-function melon.AF(t, name, default)
-    t["v_" .. name] = default
-
-    t[name] = function(s, v)
-        if v == nil then
-            return s["v_" .. name]
-        end
-
-        if t.__on_dirty then
-            t:__on_dirty(name, v)
-        end
-
-        s["v_" .. name] = v
-        return s
-    end
-
-    return function(...)
-        return melon.AF(t, ...)
-    end
-end
-
-----
 ---@name melon.AccessorFunc
 ----
 ---@arg (table: table) Table to add the accessor to
@@ -108,3 +38,9 @@ function melon.AccessorFunc(tbl, name, def, type)
 
     tbl[name] = (istable(def) and table.Copy(def)) or def
 end
+
+----
+---@alias melon.AccessorFunc
+---@name melon.AF
+----
+melon.AF = melon.AccessorFunc
