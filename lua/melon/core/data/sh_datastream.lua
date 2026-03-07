36ffaba10d84b116abbe4a3ee598b9926fc55b76
garryspins <orionspersonal1954@gmail.com>
2026-03-07 11:04:50 -0500

    feat: optional values in datastream:OnConsume

--- lua/melon/core/data/sh_datastream.lua
+++ lua/melon/core/data/sh_datastream.lua
@@ -54,19 +54,24 @@ end
 
 ----
 ---@method
----@name melon.DATASTREAM:Length
+---@name melon.DATASTREAM:OnConsume
 ----
----@arg    (number) Index consumed
----@return (any) Value consumed
+---@arg    (index: number) Key/index where we are
+---@arg    (any) Any value that got consumed
+---@arg    (nohook: bool) Was this called with nohook?
+---@return (any) New return value for Consume
 ----
 ---- Called whenever a value is consumed from the stream
 ---- This is useful for tokenizers where you wanna track characters sent through
 ----
 ---- Note that this is intended to be overriden
 ----
-function DS:OnConsume(k, v)
-    if isstring(self:GetData()) then return end
+function DS:OnConsume(k, v, nohook)
+    if isstring(self:GetData()) then return v end
+    if nohook then return v end
     self:Set(k, nil)
+
+    return v
 end
 
 ----
@@ -148,17 +153,18 @@ end
 ---@method
 ---@name melon.DATASTREAM:Consume
 ----
+---@arg    (nohook: bool) Should we skip calling OnConsume?
 ---@return (any) Any data we consumed
 ----
 ---- Consumes the current value and returns it
 ---- This increments the index we are currently on and calls [melon.DATASTREAM:OnConsume]
 ----
-function melon.DATASTREAM:Consume()
+function melon.DATASTREAM:Consume(nohook)
     local k = self:GetIndex()
     local v = self:Get(k)
 
     self:SetIndex(k + 1)
-    self:OnConsume(k, v)
+    v = self:OnConsume(k, v, nohook)
 
     return v
 end
@@ -168,15 +174,16 @@ end
 ---@name melon.DATASTREAM:ConsumeN
 ----
 ---@arg    (number) Number of values to consume
+---@arg    (nohook: bool) Should we skip calling OnConsume?
 ---@return (table<any>) Any data we consumed
 ----
 ---- Consumes the given number of values
 ----
-function melon.DATASTREAM:ConsumeN(n)
+function melon.DATASTREAM:ConsumeN(n, nohook)
     local data = {}
 
     for i = 1, n do
-        table.insert(data, self:Consume())
+        table.insert(data, self:Consume(nohook))
     end
 
     return data
@@ -186,10 +193,11 @@ end
 ---@method
 ---@name melon.DATASTREAM:PeekUntil
 ----
----@arg    (fn(any, number) -> bool) Function that determines if we should stop
+---@arg    (fn(any, number) -> bool, ...any) Function that determines if we should stop
 ---@arg    (number?) Index to start at relative to the current index
 ---@return (table<number, any>) Data that we got
 ---@return (bool) Did we hit the end of the stream?
+---@return (...any) Any extra data returned from the function
 ----
 ---- Peeks at the datastream until the given `function` returns `true`
 ---- The returned data does not include the final piece of data!
@@ -199,19 +207,21 @@ function DS:PeekUntil(fn, n)
     n = n or 0
 
     local data = {}
-
+    local ret = {}
     while self:Peek(n) do
         local v = self:Peek(n)
 
-        if fn(v, i) then
-            return data, false
+        ret = {fn(v, n)}
+        local pass = table.remove(ret, 1)
+        if pass then
+            return data, false, unpack(ret)
         end
 
         table.insert(data, v)
         n = n + 1
     end
 
-    return data, true
+    return data, true, unpack(ret)
 end
 
 ----
@@ -219,24 +229,60 @@ end
 ---@name melon.DATASTREAM:ConsumeUntil
 ----
 ---@arg    (fn(any, number) -> bool) Function that determines if we should stop
----@arg    (number?) Index to start at relative to the current index
+---@arg    (nohook: bool) Should we skip calling OnConsume?
 ---@return (table<number, any>) Data that we got
 ---@return (bool) Did we hit the end of the stream?
 ----
 ---- Consumes data from the stream until the given `function` returns `true`
 ---- If the function hits the end of the stream before the function can return true, we return true as the second return
 ----
-function DS:ConsumeUntil(fn, n)
-    local val, hit = self:PeekUntil(fn, n)
-    self:ConsumeN(#val)
+function DS:ConsumeUntil(fn, nohook)
+    n = n or 0
 
-    return val, hit
+    local data = {}
+    local ret = {}
+    while self:Peek() do
+        local v = self:Peek()
+
+        ret = {fn(v, n)}
+        local pass = table.remove(ret, 1)
+        if pass then
+            return data, false, unpack(ret)
+        end
+
+        table.insert(data, self:Consume(nohook))
+        n = n + 1
+    end
+
+    return data, true, unpack(ret)
 end
 
+----
+---@method
+---@name melon.DATASTREAM:PeekIs
+----
+---@arg    (any) Any value to compare against
+---@arg    (number?) The index to peek at, relative to where we are
+---@return (bool) Is the next value the given value?
+----
+---- [melon.DATASTREAM:Peek]s at a value and compares against it
+---- Note that this is intended to be overriden for other datatypes, as it only supports strings by default
+----
+function melon.DATASTREAM:PeekIs(is, n)
+    for ch, i in melon.str.Chars(is) do
+        if self:Peek((n or 0) + (i - 1)) != ch then
+            return false
+        end
+    end
+
+    return true
+end
 
 melon.Debug(function()
-    local ds = melon.NewDataStream("abcdefghijklmnopqrstuvwxyz")
+    local ds = melon.NewDataStream("abcdefghi")
 
-    _p(ds:ConsumeUntil(function(ch) return ch == "f" end))
-    _p(ds:Peek())
+    _p(ds:ConsumeUntil(function(ch, i)
+        return ch == "f" and ds:Peek(1) == "g"
+    end))
+    _p(ds:PeekIs("g"))
 end, true)
