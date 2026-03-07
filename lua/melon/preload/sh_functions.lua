f0bff8f24b40fea21461da766ae237442b96b242
garryspins <orionspersonal1954@gmail.com>
2026-03-07 11:01:54 -0500

    feat: melon.fn.jitinfo

--- lua/melon/preload/sh_functions.lua
+++ lua/melon/preload/sh_functions.lua
@@ -41,17 +41,46 @@ function melon.fn.info(fn)
     return infocache[fn]
 end
 
-melon.Debug(function()
-    local add = function(a, b)
-        return a + b
-    end
+----
+---@dataclass
+---@name LuaJITFnInfo
+----
+---@value (linedefined: number)     Line this function is defined on
+---@value (lastlinedefined: number) Last line this function is defined on
+---@value (params: number)          How many parameters this function takes
+---@value (stackslots: number)      How many stack slots this functions locals take
+---@value (upvalues: number)        How many upvalues this function uses
+---@value (bytecodes: number)       How many bytecodes this function has
+---@value (gcconsts: number)        How many garbage collectible constants
+---@value (nconsts: number)         How many lua_Number (double) constants
+---@value (children: bool)          Does this function create closures
+---@value (currentline: number)     What line are we on
+---@value (isvararg: bool)          Does this function use varargs
+---@value (source: string)          What file this function is defined on
+---@value (loc: string)             A string formatted like "<source>:<line>"
+---@value (ffid?: number)           Fast function ID if this is a C function
+---@value (addr?: number)           Address if this is a C function
+----
+---- Return from [melon.fn.jitinfo] and [jit.util.funcinfo]
+----
 
-    add = melon.fn.detour(add, function(f, a, b)
-        return f(a, b * 2)
-    end )
+local jitinfocache = {}
+----
+---@name melon.fn.jitinfo
+----
+---@arg    (fn) Any function to get data from
+---@return (DebugInfo) The debuginfo from it
+----
+---- Returns cached [jit.util.funcinfo] with all params
+---- This is substantially faster that [jit.util.funcinfo] for repeated operations
+----
+function melon.fn.jitinfo(fn)
+    if jitinfocache[fn] then return jitinfocache[fn] end
+    local info = jit.util.funcinfo(fn)
 
-    print(add(1, 2))
-end )
+    jitinfocache[fn] = info
+    return jitinfocache[fn]
+end
 
 melon.Debug(function()
     local f = function(a, b, c)
@@ -68,4 +97,14 @@ melon.Debug(function()
     _p(melon.Profile(iters, function()
         return melon.fn.info(f)
     end) .. 's')
-end )
+
+    _pname("jit.util.funcinfo: " .. iters)
+    _p(melon.Profile(iters, function()
+        return jit.util.funcinfo(f)
+    end) .. 's')
+
+    _pname("melon.fn.jitinfo: " .. iters)
+    _p(melon.Profile(iters, function()
+        return melon.fn.jitinfo(f)
+    end) .. 's')
+end, true)
